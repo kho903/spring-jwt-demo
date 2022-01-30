3b043b85dd391293afc1f5c51928c17ac24c4742
kho903 <gmldnr2222@naver.com>
2022-01-30 23:03:23 +0900

    feat: add JWT Code, Security Setting

--- src/main/java/com/example/jwtdemo/config/SecurityConfig.java
+++ src/main/java/com/example/jwtdemo/config/SecurityConfig.java
@@ -1,12 +1,37 @@
 package com.example.jwtdemo.config;
 
+import com.example.jwtdemo.jwt.JwtAccessDeniedHandler;
+import com.example.jwtdemo.jwt.JwtAuthenticationEntryPoint;
+import com.example.jwtdemo.jwt.JwtSecurityConfig;
+import com.example.jwtdemo.jwt.TokenProvider;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true) // @PreAuthorize 어노테이션을 메소드단위로 추가하기 위해 적용
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final TokenProvider tokenProvider;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+
+    public SecurityConfig(TokenProvider tokenProvider, JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint, JwtAccessDeniedHandler jwtAccessDeniedHandler) {
+        this.tokenProvider = tokenProvider;
+        this.jwtAuthenticationEntryPoint = jwtAuthenticationEntryPoint;
+        this.jwtAccessDeniedHandler = jwtAccessDeniedHandler;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Override
     public void configure(WebSecurity web) throws Exception {
@@ -21,8 +46,28 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .csrf().disable()
+
+                .exceptionHandling()
+                .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                .accessDeniedHandler(jwtAccessDeniedHandler)
+
+                .and()
+                .headers()
+                .frameOptions()
+                .sameOrigin()
+
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
+                .and()
                 .authorizeRequests() // HttpServletRequest 를 사용하는 요청들에 대한 접근제한 설정
                 .antMatchers("/api/hello").permitAll() // "/api/hello"에 대한 요청은 인증없이 접근허용
-                .anyRequest().authenticated(); // 나머지 요청은 인증 받아야 함
+                .antMatchers("/api/authenticate").permitAll()
+                .antMatchers("/api/signup").permitAll()
+                .anyRequest().authenticated() // 나머지 요청은 인증 받아야 함
+                .and()
+                .apply(new JwtSecurityConfig(tokenProvider));
     }
 }
